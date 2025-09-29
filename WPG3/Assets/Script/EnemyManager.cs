e1f1b620f0ff2902bf602a615d3ee161e93a1c6e
Danicoolas <daniwtc1987@gmail.com>
2025-09-29 21:32:01 +0700

    Map and lainnya

--- WPG3/Assets/Script/EnemyManager.cs
+++ WPG3/Assets/Script/EnemyManager.cs
@@ -6,7 +6,12 @@ public class EnemyManager : MonoBehaviour
     public static int aliveEnemies = 0;
     public static Action<int> OnAliveEnemiesChanged;
 
-
+    public static void ChangeAlive(int delta)
+    {
+        aliveEnemies += delta;
+        if (aliveEnemies < 0) aliveEnemies = 0;
+        OnAliveEnemiesChanged?.Invoke(aliveEnemies);
+    }
     public static void ResetManager()
     {
         aliveEnemies = 0;
